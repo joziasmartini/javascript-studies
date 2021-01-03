4036db7ee3231ec6f16671c90790443dc6df7f3f
Jozias Martini <jozias.martini@gmail.com>
2021-01-03 13:30:19 -0300

    Transform rock, paper, scissors to pug, sass, js files

---
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -0,0 +1,85 @@
+const message = document.querySelector('.message');
+const score = document.querySelector('.score');
+const buttons = document.querySelectorAll('button');
+const winnerScores = [0,0];
+
+//Add event listeners to buttons
+for ( let i = 0 ; i < buttons.length ; i++){
+    buttons[i].addEventListener('click', playGame);
+}
+
+function playGame(e){
+    //Setup player selection
+    let playerSelection = e.target.innerText;
+    //Setup a random number to select for computer
+    //Selects a number between 0 and 1
+    let computerSelection = Math.random();
+
+    //If computerSelection is less than .34, computer selects Rock
+    if (computerSelection < .34){
+        computerSelection = 'Rock';
+    } else if (computerSelection <= .67){
+        computerSelection = 'Paper';
+    } else {
+        computerSelection = 'Scissors';
+    }
+    
+    //Setup a function to compare winners and return result
+    let result = checkWinner(playerSelection, computerSelection);
+
+    //Output scores to the dom
+    if (result === 'Player'){
+        result += ' wins!';
+        //Update score
+        winnerScores[0]++;
+    }
+
+    if (result === 'Computer'){
+        result += ' wins!';
+        winnerScores[1]++;
+    }
+
+    if (result === 'Draw'){
+        result += '. It\'s a tie!'
+    }
+
+    //Output score into score div
+    score.innerHTML = 'Player: [ ' + winnerScores[0]+ ' ] Computer: [ ' + winnerScores[1] + ' ]';
+
+    //Output player and computer selections
+    messenger('Player: <strong>' + playerSelection + '</strong> Computer: <strong>' + computerSelection + '</strong><br>' + result);
+}
+
+function messenger(selectionMessage){
+    message.innerHTML = selectionMessage;
+}
+
+function checkWinner(player, computer){
+    if (player === computer){
+        return 'Draw';
+    }
+
+    if (player === 'Rock'){
+        if(computer === 'Paper'){
+            return 'Computer';
+        } else {
+            return 'Player';
+        }
+    }
+
+    if (player === 'Paper'){
+        if (computer === 'Scissors'){
+            return 'Computer';
+        } else {
+            return 'Player';
+        }
+    }
+
+    if (player === 'Scissors'){
+        if (computer === 'Rock'){
+            return 'Computer';
+        } else {
+            return 'Player';
+        }
+    }
+}
